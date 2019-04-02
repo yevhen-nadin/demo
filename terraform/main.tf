7b05d4914cbc85cc19a6eb73d0a6d4dd65748a7a
yevhen-nadin <yenadin@gmail.com>
2019-04-02 17:27:18 +0300

    Update main.tf
    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_network_security_rule" "SSH_rule" {
     name                       = "SSH"
     priority                   = 1001
     direction                  = "Inbound"
-    access                     = "Allow"
+    access                     = "Deny"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"

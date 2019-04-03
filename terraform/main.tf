12b12bae75c2547dacd95b0f4b5fd7ed322c64a4
yevhen-nadin <yenadin@gmail.com>
2019-04-03 13:57:31 +0300

    Update main.tf
    allow public access

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,7 +60,7 @@ resource "azurerm_network_security_rule" "SSH_rule" {
     name                       = "SSH"
     priority                   = 1001
     direction                  = "Inbound"
-    access                     = "Deny"
+    access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
@@ -74,7 +74,7 @@ resource "azurerm_network_security_rule" "WEB_rule" {
     name                       = "WEB_80"
     priority                   = 1002
     direction                  = "Inbound"
-    access                     = "Deny"
+    access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"

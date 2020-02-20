187523acf191dae5787d8257e7e657c994fabc7b
Yevhen Nadin <Yevhen_Nadin@epam.com>
2020-02-20 17:48:13 +0200

    added ability to show demo page on EC2

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,20 +2,47 @@ provider "aws" {
   region = "eu-central-1"
 }
   
-resource "aws_instance" "test_VM" {
-  # Amazon Linux AMI 2017.03.1 (HVM)
-  ami           = "ami-09def150731bdbcc2"
-  instance_type = "t2.micro"
-  
-  associate_public_ip_address = true
-  
-  user_data = "${file("userdata.sh")}"
-
-  tags {
-    Name = "${var.vm_name}"
-  }
-}
 
 variable "vm_name" {
   description = "Name for VM to be created"
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_all_http_demo"
+  description = "Allow HTTP inbound traffic for Demo purpose"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
+   tags = {
+    Name = "allow_all"
+	env = "Demo AWS via Terraform"
+  }
+
+}
+
+resource "aws_instance" "test_VM" {
+	ami = "ami-0b418580298265d5c"
+	instance_type = "t2.micro"
+	#key_name = "deployer-key"
+	
+	security_groups = ["${aws_security_group.allow_http.name}"]
+
+    user_data = "${file("userdata.sh")}"
+
+    tags {
+        Name = "${var.vm_name}"	
+		env = "Demo AWS via Terraform"
+	}
 }

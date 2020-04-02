3b998155f07d37c117684a54379fb80abb0abe14
yevhen-nadin <yenadin@gmail.com>
2020-04-02 17:48:08 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}

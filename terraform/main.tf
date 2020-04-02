c6d5a1cb352b97e4c714da9d379b026c8fd7bbd1
yevhen-nadin <yenadin@gmail.com>
2020-04-02 17:56:15 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "data.aws_ami.ubuntu.id"
   instance_type = "t2.micro"
 
   tags = {

145989af3b04c345682a63214b5c975c84f4efb0
Yevhen Nadin <Yevhen_Nadin@epam.com>
2020-09-03 17:52:37 +0300

    Fixed ports

--- aws-zabbix/main.tf
+++ aws-zabbix/main.tf
@@ -52,8 +52,8 @@ resource "aws_security_group" "zabbix" {
   }
 
   ingress {
-    from_port   = 8080            #to app
-    to_port     = 8080
+    from_port   = 80            #to app
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

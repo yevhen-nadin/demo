b7aaa405076d9927ac2844e7fafbad74a5c4ac8b
Yevhen Nadin <Yevhen_Nadin@epam.com>
2020-09-03 17:31:10 +0300

    Added broken port

--- aws-zabbix/main.tf
+++ aws-zabbix/main.tf
@@ -52,8 +52,8 @@ resource "aws_security_group" "zabbix" {
   }
 
   ingress {
-    from_port   = 80            #to app
-    to_port     = 80
+    from_port   = 8080            #to app
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

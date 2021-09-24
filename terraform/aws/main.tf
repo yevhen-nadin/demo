53e9c6fb8e27a3c06d9a310396b476f2a2ef138d
yevhen-nadin <yenadin@gmail.com>
2021-09-24 18:06:54 +0300

    Update main.tf
    adsfsdf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "eu-central-1" # "us-east-2"
+  region = var.vm_region
   # version = "v2.70.0"
 }
   
 resource "aws_instance" "test_VM" {
   # Amazon Linux AMI 2017.03.1 (HVM)
-  ami           = "ami-07df274a488ca9195" # us-esat-2 "ami-00dfe2c7ce89a450b"
+  ami           = var.vm_ami # us-east-2 "ami-00dfe2c7ce89a450b" eu-central-1 "ami-07df274a488ca9195"
   instance_type = "t2.micro"
   
   associate_public_ip_address = true
@@ -20,4 +20,13 @@ resource "aws_instance" "test_VM" {
 variable "vm_name" {
   default = "demo1"
   description = "Name for VM to be created"
+}
+
+variable "vm_region" {
+  default = "us-east-2"
+  description = "Default region"
+}
+
+variable "vm_ami" {
+   default = "ami-00dfe2c7ce89a450b"
 }

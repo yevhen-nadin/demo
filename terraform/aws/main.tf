d45ced77dec3bdae1512075c73c6f5bda152b38e
yevhen-nadin <yenadin@gmail.com>
2021-09-24 17:59:40 +0300

    Update main.tf
    region

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-central-1" # "us-east-2"
   # version = "v2.70.0"
 }
   
 resource "aws_instance" "test_VM" {
   # Amazon Linux AMI 2017.03.1 (HVM)
-  ami           = "ami-00dfe2c7ce89a450b"
+  ami           = "ami-07df274a488ca9195" # us-esat-2 "ami-00dfe2c7ce89a450b"
   instance_type = "t2.micro"
   
   associate_public_ip_address = true
